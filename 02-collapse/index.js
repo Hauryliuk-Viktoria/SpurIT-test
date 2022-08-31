98702041cf5f6ab4c8d6ed752baf377393105ae6
Viktoria <81378781+Hauryliuk-Viktoria@users.noreply.github.com>
2022-08-31 17:49:54 +0300

    Update index.js

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -15,27 +15,27 @@ document.querySelector(".collapsible__action--visible").style.display = 'none';
 
 
 document.querySelector("button").addEventListener("click", () => {
-  console.log("button clicked");
-  console.log("actual state is --{}", anim.playState);
+//   console.log("button clicked");
+//   console.log("actual state is --{}", anim.playState);
   if (anim.playState === "paused") {
     document.querySelector(".collapsible__content").style.visibility = 'visible';
     anim.play();
     document.querySelector(".collapsible__action--visible").style.display = 'block';
     document.querySelector(".collapsible__action--hidden").style.display = 'none';
 
-    console.log("1state after lgic is --> {}", anim.playState);
-    anim.playState="running";
-    console.log("1state after lgic is --> {}", anim.playState);
+//     console.log("1state after lgic is --> {}", anim.playState);
+//     anim.playState="running";
+//     console.log("1state after lgic is --> {}", anim.playState);
   } else {
     document.querySelector(".collapsible__action--hidden").style.display = 'block';
     document.querySelector(".collapsible__action--visible").style.display = 'none';
 
     document.querySelector(".collapsible__content").style.visibility = 'hidden';
-    console.log("2state after lgic is --> {}", anim.playState);
+//     console.log("2state after lgic is --> {}", anim.playState);
     anim.pause();
-    console.log("3state after lgic is --> {}", anim.playState);
+//     console.log("3state after lgic is --> {}", anim.playState);
   }
 });
 
-console.log("4state after lgic is --> {}", anim.playState);
+// console.log("4state after lgic is --> {}", anim.playState);
 
